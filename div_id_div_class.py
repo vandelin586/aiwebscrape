91d66c0a036aed69aa0c6cff7e575a62199da309
Ryan Vandelinder <vandelinderrpm@yahoo.com>
2023-06-23 22:51:19 -0400

    'enter new url , comments show where to start looking at classes'

--- div_id_div_class.py
+++ div_id_div_class.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'Your URL here'
+url = 'https://www.michiganlottery.com/games/mega-millions'
 response = requests.get(url)
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-divs = soup.find_all('div', class_='className')
+divs = soup.find_all('div id', class_='circle light')
+
 for div in divs:
     id = div.get('id')
-    content = div.text
+    content = id.text
+    print(content)
     print(f'id: {id}, content: {content}')
--- lotteryscrape2v.0.0.1.py
+++ lotteryscrape2v.0.0.1.py
@@ -11,3 +11,6 @@ print('Winning Numbers debug:', winning_numbers)
 numbers = winning_numbers.find_all('li')
 numbers_list = [number.text for number in numbers]
 print('Winning Numbers:', numbers_list)
+
+
+#xpath to what we want to start at /html/body/main/div[3]/div/div[2]/div[1]/table/tbody/tr[1]/td[1] | element <td class="c-result-card__result c-result-card__result--has-extra"><div class="c-result-card__result-row"><ul class="c-result c-result-card__result-list    "><li class="c-ball c-result__item   c-ball--default"><span class="c-ball__label">6</span></li><li class="c-ball c-result__item   c-ball--default"><span class="c-ball__label">37</span></li><li class="c-ball c-result__item   c-ball--default"><span class="c-ball__label">39</span></li><li class="c-ball c-result__item   c-ball--default"><span class="c-ball__label">45</span></li><li class="c-ball c-result__item   c-ball--default"><span class="c-ball__label">46</span></li><li class="c-result__item c-result__bonus-ball"><span class="c-ball c-ball--yellow c-ball--  ">21</span><abbr class="c-result__abbr  " title="Mega Ball">MB</abbr></li><li class="c-result__multiplier  ">Megaplier: x4</li></ul></div></td>
